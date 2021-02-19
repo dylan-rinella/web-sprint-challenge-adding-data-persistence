2085a4b8656cd6bcd0087d96b5e62b248ec00cc0
dylan-rinella <rinelladylan@aim.com>
2021-02-19 13:09:04 -0500

    mvp.. auto tests suck.. everything works

--- api/server.js
+++ api/server.js
@@ -2,11 +2,13 @@ const express = require('express')
 
 const projectRouter = require('../api/project/project-router')
 const resourceRouter = require('../api/resource/resource-router')
+const taskRouter = require('./task/task-router')
 
 const server = express();
 
 server.use(express.json());
 server.use('/api/projects', projectRouter)
 server.use('/api/resources', resourceRouter)
+server.use('/api/tasks', taskRouter)
 
 module.exports = server;
